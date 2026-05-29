a2e74404903e58ddcee38da3d2921735d41f632c
sushiiyay <nfa.aliya7811@gmail.com>
2026-05-29 15:37:57 +0700

    Menambahkan void display

--- queue.cpp
+++ queue.cpp
@@ -47,4 +47,30 @@ public:
     }
 
     void remove()
+    {
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else
+        {
+            // jika elemen yang dihapus berada di posisi terahir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
+    }
+
+    void display()
 }
