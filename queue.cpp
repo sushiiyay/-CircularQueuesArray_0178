73fcf06aa588b0f3ae88fc7ea8e4a445f1e724a6
sushiiyay <nfa.aliya7811@gmail.com>
2026-05-29 15:31:19 +0700

    Menambahkan else

--- queue.cpp
+++ queue.cpp
@@ -19,7 +19,23 @@ public:
     {
         int num;
         cout << "Enter a number: ";
-        cin >>
+        cin >> nim;
+        cout << endl;
 
+        //cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
+        //cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else
+        {}
     }
 }
