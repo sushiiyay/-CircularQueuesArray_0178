98724191d35eecb801b2fe831b9317fde7fc28d8
sushiiyay <nfa.aliya7811@gmail.com>
2026-05-29 15:32:33 +0700

    Menambahkan if

--- queue.cpp
+++ queue.cpp
@@ -36,6 +36,11 @@ public:
             REAR = 0;
         }
         else
-        {}
+        {
+            // jika rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max -1)
+                REAR = 0;
+            else
+        }
     }
 }
